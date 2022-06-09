ff2b832374c08154d2ac1704919c4a5d3379f00f
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-06-08 21:13:43 -0300

    Alters

---
+++ src/tipo/controller/list-tipo.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    $('#table-tipo').DataTable({
+        "processing":true,
+        "serverSide":true,
+        "ajax": {
+            "url": "src/tipo/model/list-tipo.php",
+            "type": "POST"
+
+        },
+        "column": [{
+            "data" : 'ID',
+            "classname": 'text-center'
+        },
+        {
+            "data": 'NOME',
+            "classname": 'text-center'
+        },
+        {
+            "data" : 'ID',
+            "classname": 'text-center',
+            "orderable": false,
+            "searchable": false,
+            "render": function(data, type, row, meta){
+                return `
+                <button id="${data}" class="btn btn-info btn-view">Vizualizar</button>
+                <button id="${data}" class="btn btn-warning btn-edit">Editar</button>
+                <button id="${data}" class="btn btn-danger btn-delete">Excluir</button>
+                `
+            }
+
+
+        }
+    ]
+
+
+    })
+
+})
